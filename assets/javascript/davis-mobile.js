9b31e88accb5438ce1592bae1aa487f82fd66834
Davis Birsan <davis.birsan@gmail.com>
2019-06-11 21:41:36 -0500

    more work on UI/UX

--- assets/javascript/davis-mobile.js
+++ assets/javascript/davis-mobile.js
@@ -3,6 +3,7 @@ var userName = "";
 var crushName = "";
 var dateCity = "";
 var faveFood = "";
+var percentage = "";
 
 $("#hide-age").on("click", function () {
     userAge = $("#userAge").val().trim();
@@ -29,8 +30,50 @@ $("#hide-crush-name").on("click", function () {
     $("#cityObject").addClass("slideLeft");
 });
 $("#hide-date-city").on("click", function () {
+    dateCity = $("#dateCity").val().trim();
+    console.log(dateCity);
     $("#cityObject").attr("data-visibility", "hidden");
     $("#cityObject").hide();
     $(".hide-me-food").show();
     $("#foodObject").addClass("slideLeft");
 });
+$("#hide-fave-food").on("click", function() {
+faveFood = $("#faveFood").val().trim();
+console.log(faveFood);
+$("#foodObject").hide();
+loader();
+loveCalculator();
+});
+
+
+function loader() {
+    $(".spinner").show();
+    setTimeout(function () { 
+        $(".spinner").hide();
+    }, 5000);
+ }
+
+ function loveCalculator() {
+    var proxy = "https://cors-anywhere.herokuapp.com/";
+    var queryURL = "https://love-calculator.p.rapidapi.com/getPercentage?fname=" + userName + "&sname=" + crushName;
+    $.ajax({
+        url: proxy+queryURL,
+        method: "GET",
+        headers: {"X-RapidAPI-Host": "love-calculator.p.rapidapi.com"},
+        headers: {"X-RapidAPI-Key": "e6021ca9a5msh5763cf5deefbf36p1059e8jsn2fea555b0671"},
+    }).then(function (response) {
+        console.log(response);
+        percentage = response.percentage;
+        $(".grid").show();
+        $("#percentage").text(percentage + "%")
+        $(".bar-1").css("width", percentage)
+    });
+
+ }
+
+//  function grid() {
+//      $(".grid").show();
+//      $("#percentage").text(percentage + "%")
+//      $(".bar-1").css("width", percentage)
+//   }
+
