6c01a880d1596e863bace9a98f32d0ec1333e187
Alex Wilson <alexw519@gmail.com>
2019-06-16 16:39:31 -0500

    Adding To index.html & Documenting

--- assets/javascript/davis-mobile.js
+++ assets/javascript/davis-mobile.js
@@ -1,6 +1,8 @@
 var percentage = "";
 var spacing = '\xa0\xa0';
 var userResponse = {};
+
+//Array Of The Different Questions
 var prompt = [
     {
         message: "Enter Your Age",
@@ -29,6 +31,7 @@ var prompt = [
     }
 
 ]
+
 var resetBtn = $('<input type="button" value="Not the one? Try again!"/>');
 resetBtn.addClass('waves-effect').addClass('waves-light').addClass('btn').addClass('lighten-2').addClass('hoverable');
 var resultsBtn = $('<input type="button" value="I\'ll risk it"/>');
@@ -37,9 +40,6 @@ var i =0;
 
 
 function displayPrompt(arr, i) {
-    console.log('display promt working');
-
-        // console.log('arr', arr)
     var promptBox = 
         `<div id="ageObject" class="slideLeft">
     ${arr[i].message}
@@ -52,15 +52,12 @@ function displayPrompt(arr, i) {
 }
 
 var next = true;
-
 var box = $('<div>')
-
 $(".myContainer").append(box);
-
 box.html(displayPrompt(prompt, i)) 
 
+//Goes To The Next Question When Clicked
 $(document).on("click", "#next-question",function () {
-    console.log(i);
     if($(`#${prompt[i].id}`).val().trim() == ''){
         $(`#${prompt[i].id}`).addClass("error")
       
@@ -74,19 +71,19 @@ $(document).on("click", "#next-question",function () {
         i++;
         var res = displayPrompt(prompt, i);
         box.html(res)
-        console.log(userResponse)
     } else if (i=4){
         userResponse[prompt[i].id] = $(`#${prompt[i].id}`).val().trim();
         box.hide();
         loveCalculator();
     }
- 
 });
 
+//Function To Call The Love Calculator API
 function loveCalculator() {
 
     var {userName, crushName} = userResponse;
 
+    //Shows The Loading Screen
     $(".spinner").show();
     var proxy = "https://cors-anywhere.herokuapp.com/";
     var queryURL = "https://love-calculator.p.rapidapi.com/getPercentage?fname=" + userName + "&sname=" + crushName;
@@ -96,6 +93,8 @@ function loveCalculator() {
         headers: {"X-RapidAPI-Host": "love-calculator.p.rapidapi.com"},
         headers: {"X-RapidAPI-Key": "e6021ca9a5msh5763cf5deefbf36p1059e8jsn2fea555b0671"},
     }).then(function (response) {
+
+        //Displays The Percentage On A Graph And Calls The Map Function
         $(".spinner").hide();
         $(".fallingHearts").hide();
         console.log(response);
@@ -108,6 +107,8 @@ function loveCalculator() {
         output();
         placeMap();
     }).fail(function(xhr) {
+
+        //If The Call Fails, Generates A Random Number And Displays The Results
         $(".spinner").hide();
         $(".fallingHearts").hide();
         var errorMessage = xhr.status + ': ' + xhr.statusText
@@ -165,6 +166,8 @@ function iceCreamSuggest()
     })
 }
 
+
+//Suggests A Nearby Bar
 function barSuggest()
 {
     var localURL = "http://ip-api.com/json/";
@@ -177,6 +180,7 @@ function barSuggest()
     })
 }
 
+//Function To Put A Map On The Screen Based Upon Results
 function callMap(faveFood, dateCity)
 {
     $("#googleMap").html("<iframe width='450' height='350' frameborder='0' style='border:0' src='https://www.google.com/maps/embed/v1/search?q=" + faveFood + "+in+" + dateCity + "&key=AIzaSyDNR4NPh6CTtgRWlpI-HSMop8makDVAMDM&zoom=15' allowfullscreen></iframe>");
@@ -189,7 +193,6 @@ $('#resetBtn').click(function() {
 $('#resultsBtn').click(function(){
     event.preventDefault();
     placeMap();
-    console.log("Button Clicked");
 })
 
 // $(document).on("click", ".optionBtn", function () {
